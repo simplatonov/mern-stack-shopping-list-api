d9e93fbfafa48e54cd28cff5f1a8f6142ad2f526
platonovsimeon <simeon.platonov@gmail.com>
2018-07-24 14:20:54 +0300

    Version 1.0.0

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+
+const items = require("./routes/API/items");
+const cors = require("cors");
+const app = express();
+
+//Cors
+app.use(cors());
+
+//bodyParser
+app.use(bodyParser.json());
+
+//DB config
+const db = require("./config/keys").mongoURI;
+
+//Connect to the database
+mongoose.connect(db,{ useNewUrlParser: true }).then(()=>console.log("MongoDB connected...")).catch(err=>console.log(err));
+
+//Use routes
+app.use("/api/items",items);
+
+const port = process.env.PORT || 5000;
+
+app.listen(port,console.log(`Server started on port ${port}`));
